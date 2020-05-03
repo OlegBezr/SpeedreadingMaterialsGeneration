81351b2ee76cb989da28cc649929152c603b4b66
OlegBezr <obezrukavnikov@gmail.com>
2020-05-03 15:00:10 +0300

    Triangle table generation added. Now works only for table_idth == 11

--- tables.py
+++ tables.py
@@ -3,11 +3,15 @@ import convertapi
 from random import shuffle
 
 convertapi.api_secret = 'Qv4zM1tcFDkYDkza'
-schulte_templates = 'schulte.xlsx'
-changed_file = 'changed.xlsx'
+schulte_templates = 'schulte_tables.xlsx'
 template_3x3 = "template_3x3"
 template_5x5 = "template_5x5"
-columns = [chr(i+65) for i in range(20)]
+
+#!! max width in this template: 11
+triangle_templates = 'triangle_tables.xlsx'
+changed_file = 'changed.xlsx'
+
+columns = [chr(i+65) for i in range(26)]
 
 def getSchulteValues(table_size):
   values = [i + 1 for i in range(0, table_size * table_size)]
@@ -23,7 +27,6 @@ def getSchulteValues(table_size):
 def saveSchulteTable(table_size, rows, name):
   wb = load_workbook(filename = schulte_templates)
   template_name = ''
-  #saving to Excel format
   if (table_size == 3):
     template_name = template_3x3
   elif (table_size == 5):
@@ -37,14 +40,50 @@ def saveSchulteTable(table_size, rows, name):
 
   wb.save(changed_file)
   real_name = '{}.pdf'.format(name)
+  # converting to PDF
   result = convertapi.convert('pdf', { 'File': changed_file, 'WorksheetName': template_name })
   result.file.save(real_name)
 
   return real_name
 
 def getSchulteTable(table_size, name):
-  rows = getValues(table_size)
-  table = saveTable(table_size, rows, name)
+  rows = getSchulteValues(table_size)
+  table = saveSchulteTable(table_size, rows, name)
   return table
 
-print(getTable(3, '3x3'))
+
+def getTriangleValues(type, table_width):
+  if type == 'numbers':
+    numb = [i + 1 for i in range(table_width)]
+    shuffle(numb)
+    return numb
+  else:
+    letters = [chr(i + 65) for i in range(26)]
+    shuffle(letters)
+    letters = letters[:table_width]
+    return letters
+
+def saveTriangleTable(table_width, rows, name):
+  wb = load_workbook(filename = triangle_templates)
+  template_name = 'one'
+  ws = wb[template_name]
+
+  for i in range(table_width):
+    for j in [table_width + i + 1, table_width - i - 1]:
+      ws['{}{}'.format(columns[j], i + 1)] = rows[i]
+
+  wb.save(changed_file)
+  real_name = '{}.pdf'.format(name)
+  result = convertapi.convert('pdf', { 'File': changed_file, 'WorksheetName': template_name })
+  result.file.save(real_name)
+
+  return real_name
+
+
+def getTriangleTable(table_width, name, type):
+  rows = getTriangleValues(type, table_width)
+  table = saveTriangleTable(table_width, rows, name)
+  return table
+
+print(getTriangleTable(11, 'ok', 'numbers'))
+# print(getSchulteTable(3, '3x3'))
